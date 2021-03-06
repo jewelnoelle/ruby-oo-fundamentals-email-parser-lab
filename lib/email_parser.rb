ba6b022154e22971648a8ea9b3130e3d85b5a671
Jewel Carrier-Davis <jujicb@gmail.com>
2021-03-05 22:12:41 -0500

    commit tree 96a4b0775c2befbfa2c7d6e30726c22a0e034eed

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,23 @@
+require 'pry'
 # Build a class EmailAddressParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser
+    def initialize(string)
+        @string = string
+    end
+    
+    def parse
+        if @string.include?(",")
+            string_no_comma = @string.delete(",")
+            split_string = string_no_comma.split(" ")
+            binding.pry
+            split_string.uniq
+        else
+            string_space = @string.split(" ")
+            return string_space.uniq
+        end
+    end
+end 
